a2c885d60611a7b99619a52d212568cbbe4b3a4c
punctual-sailor-2214 <bchett286@yahoo.com>
2019-03-04 20:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,9 +54,8 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     }
-    return `That item is not in your cart.`
   }
-  // write your code here
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
