ae67ff3a428a0bd71bb2e466cd56a1d0921a960c
punctual-sailor-2214 <bchett286@yahoo.com>
2019-03-04 19:59:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ return `${item} has been added to your cart.`;
 function viewCart() {
   for (var i = 0; i < cart.length; i++) {
     if (i === 0) {
-        console.log("Your shopping cart is empty.");
+        return `Your shopping cart is empty.`;
     };
   };
 };
