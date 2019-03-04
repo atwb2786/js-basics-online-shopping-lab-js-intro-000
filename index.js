a08a578a25b9b0b51fff0e3097d818e3ba42970b
punctual-sailor-2214 <bchett286@yahoo.com>
2019-03-04 19:54:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+var b = Math.floor((Math.random() * 100) + 1);
+var a = {
+  itemName: `${item}`,
+  itemPrice: b
+};
+cart.push(a);
+return `${item} has been added to your cart.`;
+};
 
 function viewCart() {
-  // write your code here
-}
+  for (var i = 0; i < cart.length; i++) {
+    if (cart.length == 0) {
+        return `Your shopping cart is empty.`
+    };
+  };
+};
 
 function total() {
   // write your code here
