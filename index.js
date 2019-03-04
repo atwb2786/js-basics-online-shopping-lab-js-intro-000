c7ceb10eb241b1be831b611616ead07f1e67bb37
punctual-sailor-2214 <bchett286@yahoo.com>
2019-03-04 20:54:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function placeOrder(cardNumber) {
   if(cardNumber) {
     let totalCost = total();
     cart = [];
-    return `Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
   }
   // write your code here
 }
